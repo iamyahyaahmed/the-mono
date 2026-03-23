799acd75bd3b93af5d05acd275d934614edabe04
Yahya Ahmed <yahyaahmedg@gmail.com>
2026-03-23 17:41:19 +0500

    ffs

--- src/client/src/pages/SamplePage.tsx
+++
@@ -1,8 +0,0 @@
-export function SamplePage() {
-  return (
-    <section style={{ padding: '1.5rem' }}>
-      <h1>Sample route</h1>
-      <p>This page is rendered at <code>/sample</code>.</p>
-    </section>
-  )
-}
